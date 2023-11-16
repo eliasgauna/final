64c47c8a35197f4c9069697d598e88a6319cc890
FlorFuentes <flopy_200412@hotmail.com>
2023-11-16 19:33:37 -0300

    Footer funcionando

--- GymApp/src/App.jsx
+++ GymApp/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import './App.scss'
 import Titulo from './components/Titulo'
 import Contenedor from './components/Contenedor'
-
+import Footer from './components/Footer'
 function App() {
   
   return (
@@ -12,6 +12,7 @@ function App() {
       <Titulo TextoTitulo="Tu GYM App"/>
       <p>La aplicación que te permite organizar tu rutina</p>
       <Contenedor/>
+      <Footer/>
     </div>
   )
 }
---
+++ GymApp/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <div>
+      <footer>
+        <p>App realizada por</p>
+        <a href="https://www.linkedin.com/in/rodrigobombieri-dev?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">Elias Gauna | </a>
+        <a href="https://www.linkedin.com/in/florencia-fuentes-52b841263" target="_blank" rel="noopener noreferrer">Florencia Fuentes | </a>
+        <a href="https://www.linkedin.com/in/rodrigobombieri-dev?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">Rodrigo Bombieri</a>
+      </footer>
+    </div>
+  )
+}
+
+export default footer
