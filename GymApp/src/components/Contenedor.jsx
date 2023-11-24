4deda83738fe95db4b9818c56f8cb871684cc9d9
FlorFuentes <flopy_200412@hotmail.com>
2023-11-24 16:03:01 -0300

    useEffect y localStorage

--- GymApp/src/components/Contenedor.jsx
+++ GymApp/src/components/Contenedor.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import AgregarTarea from './AgregarTarea'
 import TaskList from './TaskList'
@@ -6,17 +6,33 @@ import TaskList from './TaskList'
 
 const Contenedor = () => {
     const [list, setList] = useState([]);
+
+    //Carga de datos al montar el componente
+    useEffect(() => {
+        const storedList = JSON.parse(localStorage.getItem("tasks")) || [];
+        console.log("Datos cargados desde localStorage:", storedList);
+        setList(storedList);
+    }, []);
+
+    //Guardamos los datos en el localStorage cuando la lista cambia.
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(list));
+        console.log("Guardando datos en localStorage:", list);
+    }, [list]);
+
+
+
     const handleAddItem = addItem => {
         setList([...list, addItem]);
     };
     return (
         <div>
             { }
-            <AgregarTarea handleAddItem={handleAddItem}/>
+            <AgregarTarea handleAddItem={handleAddItem} />
             { }
-            <TaskList list={list} setList={setList}/>
+            <TaskList list={list} setList={setList} />
         </div>
-    ); 
+    );
 };
 
 export default Contenedor 
